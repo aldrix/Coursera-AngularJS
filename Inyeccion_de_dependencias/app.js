02ffe000163f5c1d9ced4848815db3392a0a6e31
aldrix <aldrix_marfil@hotmail.com>
2016-09-13 23:49:20 -0400

    Agregada una version mas elegante que permite la minificacion del codigo javascript

--- Inyeccion_de_dependencias/app.js
+++ Inyeccion_de_dependencias/app.js
@@ -4,9 +4,8 @@
 angular.module('IDAplicacion', [])
 .controller('IDControlador', IDControlador);
 
-function IDControlador ($scope,
-                       $filter,
-                       $injector) {
+IDControlador.$inject = ['$scope','$filter'];
+function IDControlador ($scope, $filter) {
   $scope.nombre = "Aldrix";
 
   $scope.mayuscula = function () {
@@ -16,3 +15,7 @@ function IDControlador ($scope,
 }
 
 })();
+
+
+//Version minificada que permite tener archivos menos pesados y que se descargen mas rapido los archivos.
+//!function(){"use strict";function n(n,o){n.nombre="Aldrix",n.mayuscula=function(){var r=o("uppercase");n.nombre=r(n.nombre)}}angular.module("IDAplicacion",[]).controller("IDControlador",n),n.$inject=["$scope","$filter"]}();
